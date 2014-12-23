4746c80774c3ffdf064c3aa413d7b86efb2bd677
Hilverd Reker <me@hilverd.com>
2014-12-23 11:03:12 +0000

    Add author email address.

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(name='gomatic',
       description='API for configuring GoCD',
       url='https://github.com/SpringerSBM/gomatic',
       author='Springer Science+Business Media',
+      author_email='tools-engineering@groups.springer.com',
       license='MIT',
       packages=['gomatic'],
       install_requires=[
