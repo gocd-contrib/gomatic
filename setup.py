c1d65a70006963d8e5e0baf296c15e1577aed204
Ivan Moore <ivan@teamoptimization.com>
2015-01-07 11:48:59 +0000

    Ivan/Hilverd - next version will be 0.2.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='gomatic',
-      version='0.1.1',
+      version='0.2.0',
       description='API for configuring GoCD',
       url='https://github.com/SpringerSBM/gomatic',
       author='Springer Science+Business Media',
