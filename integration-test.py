7701a2db55fc89028d102a6a5386cb6569598416
Hilverd Reker <me@hilverd.com>
2015-02-25 11:29:03 +0000

    Hilverd/Ivan - make cleanup of Go server container work properly.

--- integration-test.py
+++ integration-test.py
@@ -4,6 +4,7 @@ from urllib2 import urlopen
 import time
 import os
 import unittest
+import sys
 
 from gomatic import *
 
@@ -28,35 +29,44 @@ class populated_go_server:
         self.gocd_version = gocd_version
 
     def __enter__(self):
-        start_go_server(self.gocd_version)
+        try:
+            start_go_server(self.gocd_version)
 
-        configurator = GoCdConfigurator(HostRestClient('localhost:8153'))
-        pipeline = configurator \
-            .ensure_pipeline_group("P.Group") \
-            .ensure_replacement_of_pipeline("more-options") \
-            .set_timer("0 15 22 * * ?") \
-            .set_git_material(
-            GitMaterial("git@bitbucket.org:springersbm/gomatic.git", material_name="some-material-name",
-                        polling=False)) \
-            .ensure_environment_variables(
-            {'JAVA_HOME': '/opt/java/jdk-1.7'}) \
-            .ensure_parameters({'environment': 'qa'})
-        stage = pipeline.ensure_stage("earlyStage")
-        job = stage.ensure_job("earlyWorm").ensure_artifacts(
-            {BuildArtifact("scripts/*", "files"), BuildArtifact("target/universal/myapp*.zip", "artifacts"),
-             TestArtifact("from", "to")}).set_runs_on_all_agents()
-        job.add_task(ExecTask(['ls']))
+            configurator = GoCdConfigurator(HostRestClient('localhost:8153'))
+            pipeline = configurator \
+                .ensure_pipeline_group("P.Group") \
+                .ensure_replacement_of_pipeline("more-options") \
+                .set_timer("0 15 22 * * ?") \
+                .set_git_material(
+                GitMaterial("git@bitbucket.org:springersbm/gomatic.git", material_name="some-material-name",
+                            polling=False)) \
+                .ensure_environment_variables(
+                {'JAVA_HOME': '/opt/java/jdk-1.7'}) \
+                .ensure_parameters({'environment': 'qa'})
+            stage = pipeline.ensure_stage("earlyStage")
+            job = stage.ensure_job("earlyWorm").ensure_artifacts(
+                {BuildArtifact("scripts/*", "files"), BuildArtifact("target/universal/myapp*.zip", "artifacts"),
+                 TestArtifact("from", "to")}).set_runs_on_all_agents()
+            job.add_task(ExecTask(['ls']))
 
-        configurator.save_updated_config(save_config_locally=True)
-        return GoCdConfigurator(HostRestClient('localhost:8153'))
+            configurator.save_updated_config(save_config_locally=True)
+            return GoCdConfigurator(HostRestClient('localhost:8153'))
+        except:
+            # Swallow exception if __exit__ returns a True value
+            if self.__exit__(*sys.exc_info()):
+                pass
+            else:
+                raise
 
     def __exit__(self, type, value, traceback):
+        print "*" * 60, "trying to clean up docker for %s" % self.gocd_version
         os.system("docker rm -f gocd-test-server-%s" % self.gocd_version)
+
 
 class IntegrationTest(unittest.TestCase):
     def test_all_versions(self):
         for gocd_version in ['13.4.0-18334', '14.4.0-1356']:
-            print "*" * 40, gocd_version
+            print "*" * 60, gocd_version
             with populated_go_server(gocd_version) as configurator:
                 self.assertEquals(["P.Group"], [p.name() for p in configurator.pipeline_groups()])
                 self.assertEquals(["more-options"], [p.name() for p in configurator.pipeline_groups()[0].pipelines()])
