d1066085df634c528961006ee9bb662fd48831c3
Ivan Moore <ivan@teamoptimization.com>
2015-02-25 17:28:14 +0000

    ivan - updated version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='gomatic',
-      version='0.3.3',
+      version='0.3.4',
       description='API for configuring GoCD',
       url='https://github.com/SpringerSBM/gomatic',
       author='Springer Science+Business Media',
