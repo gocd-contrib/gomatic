4f9f917f44a98c89c653070fdc966e991727e94d
Ivan Moore <ivan@teamoptimization.com>
2015-05-07 09:48:59 +0100

    Ivan - comment clarification

--- integration-test.py
+++ integration-test.py
@@ -94,7 +94,7 @@ def check_docker():
 
 class IntegrationTest(unittest.TestCase):
     def test_all_versions(self):
-        for gocd_version in [#'13.1.1-16714',
+        for gocd_version in [#'13.1.1-16714', # does not work with that version
                              '13.2.2-17585',
                              '13.3.1-18130',
                              '13.4.0-18334',
