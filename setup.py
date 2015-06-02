30f8d0e7378d9914c487fb5a62ef6cf20e6be3df
Ivan Moore <ivan@teamoptimization.com>
2015-06-02 10:21:03 +0100

    Ivan/Hilverd - new version (minor addition to api)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='gomatic',
-      version='0.3.11',
+      version='0.3.12',
       description='API for configuring GoCD',
       url='https://github.com/SpringerSBM/gomatic',
       author='Springer Science+Business Media',
