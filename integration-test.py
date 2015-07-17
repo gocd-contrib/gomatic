a5e8bdcbcebff43dd64cd3a77f999c44179cff16
Ivan Moore <ivan@teamoptimization.com>
2015-07-17 10:53:14 +0100

    ivan - updated integration tests for new version of GoCD

--- integration-test.py
+++ integration-test.py
@@ -103,7 +103,8 @@ class IntegrationTest(unittest.TestCase):
                              '14.2.0-377',
                              '14.3.0-1186',
                              '14.4.0-1356',
-                             '15.1.0-1863']:
+                             '15.1.0-1863',
+                             '15.2.0-2248']:
             print "*" * 60, gocd_version
             with populated_go_server(gocd_version) as configurator:
                 self.assertEquals(["P.Group"], [p.name() for p in configurator.pipeline_groups()])
