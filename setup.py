6fd72499ea693790e6cf77c8dedfe12e8754a5e6
Ivan Moore <ivan@teamoptimization.com>
2015-09-03 09:37:00 +0100

    new version - reorder template xml to avoid config thrash - i.e. to avoid changing xml in a way that makes no difference but still looks like a change

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='gomatic',
-      version='0.3.26',
+      version='0.3.27',
       description='API for configuring GoCD',
       url='https://github.com/SpringerSBM/gomatic',
       author='Springer Science+Business Media',
