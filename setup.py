ef7a5010e07487cbb4936c49844f90596bd27b1f
Ivan Moore <ivan@teamoptimization.com>
2015-10-05 15:06:39 +0100

    hilverd/ivan - bump up version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='gomatic',
-      version='0.3.30',
+      version='0.3.31',
       description='API for configuring GoCD',
       url='https://github.com/SpringerSBM/gomatic',
       author='Springer Science+Business Media',
