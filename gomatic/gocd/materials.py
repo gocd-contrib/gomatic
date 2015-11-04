4144be9df5f00d17f1185467b6a7f836c7bc32ee
Connor Shearwood <connor.shearwood@springer.com>
2015-11-04 14:17:38 +0000

    Connor - materials property refactor

--- gomatic/gocd/materials.py
+++ gomatic/gocd/materials.py
@@ -41,16 +41,18 @@ class GitMaterial(CommonEqualityMixin):
         if not self.__polling:
             polling_part = ', polling=False'
         ignore_patterns_part = ''
-        if self.ignore_patterns():
-            ignore_patterns_part = ', ignore_patterns=%s' % self.ignore_patterns()
+        if self.ignore_patterns:
+            ignore_patterns_part = ', ignore_patterns=%s' % self.ignore_patterns
         destination_directory_part = ''
         if self.destination_directory():
             destination_directory_part = ', destination_directory="%s"' % self.destination_directory()
         return ('GitMaterial("%s"' % self.__url) + branch_part + material_name_part + polling_part + ignore_patterns_part + destination_directory_part + ')'
 
+    @property
     def __has_options(self):
-        return (not self.is_on_master()) or (self.material_name() is not None) or (not self.polling()) or self.ignore_patterns() or self.destination_directory()
+        return (not self.is_on_master) or (self.material_name is not None) or (not self.polling) or self.ignore_patterns or self.destination_directory
 
+    @property
     def is_on_master(self):
         return self.__branch is None or self.__branch == 'master'
 
@@ -60,27 +62,32 @@ class GitMaterial(CommonEqualityMixin):
         else:
             return 'set_git_url("%s")' % self.__url
 
-    def is_git(self):
-        return True
+    is_git = True
 
+    @property
     def url(self):
         return self.__url
 
+    @property
     def polling(self):
         return self.__polling
 
+    @property
     def branch(self):
-        if self.is_on_master():
+        if self.is_on_master:
             return 'master'
         else:
             return self.__branch
 
+    @property
     def material_name(self):
         return self.__material_name
 
+    @property
     def ignore_patterns(self):
         return self.__ignore_patterns
 
+    @property
     def destination_directory(self):
         return self.__destination_directory
 
@@ -103,10 +110,10 @@ class GitMaterial(CommonEqualityMixin):
 
         new_element = ET.fromstring(('<git url="%s"' % self.__url) + branch_part + material_name_part + polling_part + destination_directory_part + ' />')
 
-        if self.ignore_patterns():
+        if self.ignore_patterns:
             filter_element = ET.fromstring("<filter/>")
             new_element.append(filter_element)
-            sorted_ignore_patterns = list(self.ignore_patterns())
+            sorted_ignore_patterns = list(self.ignore_patterns)
             sorted_ignore_patterns.sort()
             for ignore_pattern in sorted_ignore_patterns:
                 filter_element.append(ET.fromstring('<ignore pattern="%s"/>' % ignore_pattern))
@@ -126,8 +133,7 @@ class PipelineMaterial(CommonEqualityMixin):
         else:
             return 'PipelineMaterial("%s", "%s", "%s")' % (self.__pipeline_name, self.__stage_name, self.__material_name)
 
-    def is_git(self):
-        return False
+    is_git = False
 
     def append_to(self, element):
         if self.__material_name is None:
