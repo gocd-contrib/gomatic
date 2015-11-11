cbedb28af916f41b2708248fba8fe02e9b8188bd
Ivan Moore <ivan@teamoptimization.com>
2015-11-11 17:47:01 +0000

    ivan - job_that_has_no_artifacts_has_no_artifacts_element_to_reduce_thrash

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='gomatic',
-      version='0.4.0',
+      version='0.4.1',
       description='API for configuring GoCD',
       url='https://github.com/SpringerSBM/gomatic',
       author='Springer Science+Business Media',
