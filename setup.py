8aeb5d81013fc77385dbf3d08dd1467ab37eb1f0
Ivan Moore <ivan@teamoptimization.com>
2015-12-18 13:31:25 +0000

    ivan/hilverd - update version number - now always sorts environment variable elements

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='gomatic',
-      version='0.4.2',
+      version='0.4.3',
       description='API for configuring GoCD',
       url='https://github.com/SpringerSBM/gomatic',
       author='Springer Science+Business Media',
