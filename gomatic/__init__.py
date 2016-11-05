39cffb53a13baf02de2d2c375825f29cd41ee583
Marcin Walczyk <marcin.walczyk88@gmail.com>
2016-11-05 14:38:04 +0100

    Add PackageMaterial functionality

--- gomatic/__init__.py
+++ gomatic/__init__.py
@@ -1,6 +1,6 @@
 from gomatic.go_cd_configurator import HostRestClient, GoCdConfigurator
 from gomatic.gocd.agents import Agent
-from gomatic.gocd.materials import GitMaterial, PipelineMaterial
+from gomatic.gocd.materials import GitMaterial, PipelineMaterial, PackageMaterial
 from gomatic.gocd.pipelines import Tab, Job, Pipeline, PipelineGroup
 from gomatic.gocd.tasks import FetchArtifactTask, ExecTask, RakeTask
 from gomatic.gocd.artifacts import FetchArtifactFile, FetchArtifactDir, BuildArtifact, TestArtifact, ArtifactFor
