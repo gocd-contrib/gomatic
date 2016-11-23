ea8c2665146f19501a01561d6892dfc542062ac3
Matt Savage <matthew.savage@gmail.com>
2016-11-23 10:34:11 +0000

    Releasing 0.4.11

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='gomatic',
-      version='0.4.10',
+      version='0.4.11',
       description='API for configuring GoCD',
       url='https://github.com/SpringerSBM/gomatic',
       author='Springer Science+Business Media',
