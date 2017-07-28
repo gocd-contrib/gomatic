47353a8e73e779ddbb826e73ee03b7e3557e3a3f
Duda Dornelles <dudassdornelles@gmail.com>
2017-07-28 10:20:36 -0700

    version 0.4.15

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(name='gomatic',
-      version='0.4.14',
+      version='0.4.15',
       description='API for configuring GoCD',
       url='https://github.com/gocd-contrib/gomatic',
       author='The Gomatic Maintainers',
